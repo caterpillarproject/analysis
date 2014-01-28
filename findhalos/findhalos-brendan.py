a6ae5103e694c501aaf3cd9a95f5a01b7caa2954
bgriffen <brendan.f.griffen@gmail.com>
2014-01-28 15:09:28 -0500

    updated to specify which halos you want to look at

--- findhalos/findhalos-brendan.py
+++ findhalos/findhalos-brendan.py
@@ -1,8 +1,6 @@
 #IMPORT CORE MODULES
-#import gadgetlibomp
 import numpy as np
 import pylab as plt
-#from pylab import *
 import sys, os, platform,glob
 import matplotlib
 #IMPORT CATERPILLAR MODULES
@@ -16,14 +14,10 @@ import readsnapshots.readsnapHDF5 as rhdf5
 from brendanlib.grifflib import *
 
 #IMPORTANT STUFF
-haloid = 121869
-#LESS IMPORTANT STUFF
 hubble = 0.6711
 snapnum = 255
-topNhalos = 4
 
 basepath = determinebasepath(platform.node())
-
 listdir = glob.glob(basepath + "caterpillar/halos/H*/*_BB_*_NV3/outputs/groups_255")
 
 fig = plt.figure(figsize=(9,9))
@@ -32,12 +26,16 @@ axb = fig.add_subplot(222)
 axc = fig.add_subplot(223)
 axd = fig.add_subplot(224)
 
-
 listdir = glob.glob(basepath + "caterpillar/halos/H*/")
 
 nvlist = ["NV3"]
 halotypelist = ["BB"]
 levelmaxlist = ["LX11","LX12","LX13","LX14"]
+
+halolist = ["H121869","H140666","H147781","H173411","H1930","H21047",
+"H233776","H241932","H242183","H260266","H268422","H69786","H96624"]
+
+halolist = ["H268422"]
 
 for halo in listdir:
     masslist = []
@@ -47,19 +45,21 @@ for halo in listdir:
     npfoflist = []
     npsublist = []
 
+
     haloname = halo.split("/")[6]
-    for haloin in glob.glob(halo+"/H*"):
+    print "----------------------------------------------"
+    for haloin in glob.glob(halo+"/H*_BB_*"):
+   # for haloin in 
+        
         haloparts = haloin.split("/")[7].split("_")
         levelmax = haloparts[5]
         halotype = haloparts[1]
         nv = haloparts[7]
+        if nv in nvlist and halotype in halotypelist and levelmax in levelmaxlist and haloname in halolist:
+            outputsdir = haloin+"/outputs"
 
-        if nv in nvlist and halotype in halotypelist and levelmax in levelmaxlist:
-            try:
-                outputsdir = haloin+"/outputs"
-                print outputsdir
-                #halopath = halo.replace("/groups_255","")
-                header = rs.snapshot_header(halopath+"/snapdir_255/snap_255.0")
+            if os.path.isdir(outputsdir+"/groups_255/"):
+#               header = rs.snapshot_header(outputsdir+"/snapdir_255/snap_255.0")
                 s = readsubf.subfind_catalog(outputsdir, snapnum)
                 id = readsubf.subf_ids(outputsdir, snapnum, 0, 0, read_all=1)
                 
@@ -74,8 +74,11 @@ for halo in listdir:
                 
                 R = np.sqrt((groupx-subposx)**2 + (groupy-subposy)**2 + (groupz-subposz)**2)
                 
-                mask = (R < 0.9) & (submass <= 9e12) & (submass >= 7e11)
-        
+                mask = (R < 0.5) & (submass <= 5e12) & (submass >= 7e11)
+
+                if not mask.any():
+                    mask = (R < 0.5) & (submass <= 9e12) & (submass >= 7e11)
+
                 level = int(levelmax[2:])
         
                 if level == 11:
@@ -97,16 +100,18 @@ for halo in listdir:
                     axa.plot(posxlist,posylist,style,markeredgewidth=0)
                     axc.plot(npfoflist,npsublist,style,markeredgewidth=0)
                     axb.plot(masslist,vmaxlist,style,markeredgewidth=0)
-                    print ""
-                    print haloname
+
+                    #print ""
+                    print "++",haloin.replace("/bigbang/data/AnnaGroup/caterpillar/halos/","")
+                    print "+",levelmax,nv
+                    #haloin.replace(basepath+"caterpillar/halos/","")
                     print "MASS: %3.2e" % (submass[mask])
                     print "VMAX: %3.2f" % (s.sub_vmax[mask])
-                    print "mp: %3.2e" % header.massarr[1]*10**10
                     print "SUB-NPART: %i6" % (s.sub_len[mask])
                     print "FOF-NPART:%i6" % (s.group_len[0])
                     print "X-Y-Z: %3.2f,%3.2f,%3.2f" % (subposx[mask],subposy[mask],subposz[mask])
-            except:
-                pass
+
+#                    print "mp: %3.2e" % header.massarr[1]*10**10
 
     masslist = np.array(masslist)
     vmaxlist = np.array(vmaxlist)
@@ -115,9 +120,9 @@ for halo in listdir:
     npfoflist = np.array(npfoflist)
     npsublist = np.array(npsublist)
 
-    axa.plot(posxlist,posylist,'-')
-    axc.plot(npfoflist,npsublist,'-')
-    axb.plot(masslist,vmaxlist,'-')
+    axa.plot(posxlist,posylist,'b-')
+    axc.plot(npfoflist,npsublist,'b-')
+    axb.plot(masslist,vmaxlist,'b-')
 
 axa.set_xlabel(r'x-pos [Mpc/h]')
 axa.set_ylabel(r'y-pos [Mpc/h]')
@@ -125,5 +130,4 @@ axb.set_ylabel(r'vmax [km/s]')
 axb.set_xlabel(r'mass [M$_\odot$]')
 axc.set_ylabel(r'log$_{10}$ [# particles in subhalo]')
 axc.set_xlabel(r'log$_{10}$ [# particles in FOF group]')
-#ax.hist(np.log10(s.sub_mass[mask]*10**10),20)
 plt.show()
