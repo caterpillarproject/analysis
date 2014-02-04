c3c918f769a1d593d506067e646af063fd642e83
bgriffen <brendan.f.griffen@gmail.com>
2014-02-04 16:42:49 -0500

    added getcontamination file

--- findhalos/getcontam-brendan.py
+++ findhalos/getcontam-brendan.py
@@ -4,17 +4,27 @@ import pynbody as pnb
 from brendanlib.grifflib import makecolormap
 import readhalos.readgroup as RG
 import readhalos.readsubf as readsubf
+import readsnapshots.readsnap as rs
 from operator import itemgetter
 import glob as glob
 import os
 
-halolist = [268422,121869,233776]
-
-#halolist = [268422]
 hubble = 0.6711
+WANTHEATMAP =  False
+WANTRADIAL = True
 
 basepath = "/bigbang/data/AnnaGroup/caterpillar/halos/"
 halolist = glob.glob(basepath + "H*")
+
+def readblock(path,parttype):
+    #print "Reading block:",parttype
+    pos = rs.read_block(path,"POS ",parttype=parttype,doubleprec=False,verbose=False)
+    x = pos[:,0]
+    y = pos[:,1]
+    z = pos[:,2]
+    #print "-- number of particles:",len(x)
+    return x,y,z
+
 
 for halo in halolist:
     halos = glob.glob(halo+"/*")
@@ -22,17 +32,75 @@ for halo in halolist:
         numblocks = []
         path = haloi + "/outputs/"
         if os.path.isdir(path+'groups_255') and "LX14" in haloi:
-            print "---------------------"
+            print "--------------------------------------------------------"
             s = readsubf.subfind_catalog(path, 255)
             contamNR = s.group_contamination_count
             contamMR = s.group_contamination_mass*10**10/hubble
             minindex = min(enumerate(s.group_contamination_count[0:3]), key=itemgetter(1))[0]
-            groupmass = s.group_mass[minindex]*10**10/hubble
-            groupos = s.group_pos[minindex]
-            print haloi.replace(basepath,"")
-            print "FOF With Smallest Contam. Position:",groupos
-            print "FOF With Smallest Contam. Mass: %0.2e" % (groupmass)
-            print "Maximum Subhalo Mass: %0.2e" % (max(s.sub_mass*10**10/hubble))
-            print "Contamination Num.:",contamNR[minindex]
-            print "Contamination Mass: %0.2e" % (contamMR[minindex]/0.6711)
 
+            pcmass_contam = (s.group_contamination_mass/s.group_mass)*100.
+            allgroupmass = s.group_mass*10**10/hubble
+            # GROUPS
+            candgrouppos = s.group_pos[minindex]
+            candgroupmass = allgroupmass[minindex]
+            # SUBS
+            maxsubindex = np.argsort(s.sub_len)
+            submass = s.sub_mass[maxsubindex][-1]*10**10/hubble
+            subpos = s.sub_pos[maxsubindex][-1]
+
+            print haloi.replace(basepath,"")
+            print "FOF With Smallest Contam. Position:",candgrouppos
+            print "FOF With Smallest Contam. Mass: %0.2e" % (candgroupmass)
+            print "Subhalo Mass With Most Particles: %0.2e" % (submass)
+            print "Contamination Num.:",contamNR[minindex]
+            print "Contamination Mass: %0.2e [%3.3f pc]" % (contamMR[minindex]/0.6711,(contamMR[minindex]/candgroupmass)*100)
+
+            if WANTRADIAL:
+                rfof = np.sqrt((candgrouppos[0]-s.group_pos[:,0])**2 + (candgrouppos[1]-s.group_pos[:,1])**2 + (candgrouppos[2]-s.group_pos[:,2])**2)
+                idxrfof = np.argsort(rfof)
+                contam_n_radial = np.cumsum(contamNR[idxrfof])
+                contam_m_radial = np.cumsum(contamMR[idxrfof])
+
+                fig = plt.figure()
+                ax = fig.add_subplot(111)
+                ax.plot(rfof[idxrfof],np.log10(contam_n_radial),linestyle='-',color='b',linewidth=2)
+                ax.set_xlim([0,7])
+                ax.set_ylim([0,5])
+                ax.set_ylabel(r'$\mathrm{\Sigma\ log_{10}\ N_{CP}(<R)}$', color='b',fontsize=14)
+    
+                # set dual side axes for both mass and number contamination count
+                for tl in ax.get_yticklabels():
+                        tl.set_color('b')
+                axb = ax.twinx()
+                axb.tick_params(axis='both', which='major', labelsize=12)
+                axb.set_ylim([8,14])
+                axb.plot(rfof[idxrfof],np.log10(contam_m_radial),linestyle='-',color='r',linewidth=2)
+                axb.set_ylim([8,14])
+                for tl in axb.get_yticklabels():
+                    tl.set_color('r')
+                axb.set_ylabel(r'$\mathrm{log_{10}\ \Sigma\ M_{CP}(<R)\ [M_\odot/h]}$', color='r',fontsize=14)
+                ax.set_xlabel(r'$\mathrm{R_{FOF}\ [Mpc/h]}$',fontsize=14)
+                ax.set_title(haloi.replace(basepath,"").replace(halo.replace(basepath,"")+"/",""))
+
+            # create heatmap for % of FOF mass contaminated versus FOF group mass.
+            if WANTHEATMAP:
+                heatmap, xedges, yedges = np.histogram2d(np.log10(s.group_mass*10**10/hubble), pcmass_contam, bins=128)
+                fig = plt.figure()
+                ax = fig.add_subplot(111)
+                extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+                heatmap = np.flipud(np.rot90(heatmap))
+                sc1 = ax.imshow(np.log10(heatmap),extent = extent,cmap = 'jet', origin='lower')
+                cbar1 = fig.colorbar(sc1)
+                ax.set_title(haloi.replace(basepath,"").replace(halo.replace(basepath,"")+"/",""))
+                ax.set_ylabel('% mass contaminated')
+                ax.set_xlabel('log [FOF mass]')
+                cbar1.set_label('# FOF Groups')
+                ax.set_aspect('auto')
+        
+            if os.path.isdir(path+'snapdir_255'):
+                for parttype in xrange(2,6):
+                        x,y,z = readblock(path+'snapdir_255/snap_255',parttype=parttype)
+                        dr = np.sqrt((subpos[0]-x)**2 + (subpos[1]-y)**2 + (subpos[2]-z)**2)
+                        print "-- Particle Type %i is %3.1f kpc away from subhalo" % (parttype,min(dr)*1000.)
+
+plt.show()
