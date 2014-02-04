fbf0ddae584afd03cce81ec7dba51899ccd93def
bgriffen <brendan.f.griffen@gmail.com>
2014-02-04 14:56:42 -0500

    added getcontamination file

--- findhalos/getcontam-brendan.py
+++ findhalos/getcontam-brendan.py
@@ -18,24 +18,20 @@ halolist = glob.glob(basepath + "H*")
 
 for halo in halolist:
     halos = glob.glob(halo+"/*")
-    
     for haloi in halos:
         numblocks = []
         path = haloi + "/outputs/"
         if os.path.isdir(path+'groups_255') and "LX14" in haloi:
             print "---------------------"
             s = readsubf.subfind_catalog(path, 255)
-            #R = np.sqrt((self.xref-xpossub)**2+(self.yref-ypossub)**2+(self.zref-zpossub)**2)
             contamNR = s.group_contamination_count
             contamMR = s.group_contamination_mass*10**10/hubble
-            groupcat = RG.group_tab(path,255)
-            grouppos = groupcat.GroupCM[0,:]
-            groupmass = groupcat.GroupMass[0]*10**10/hubble
             minindex = min(enumerate(s.group_contamination_count[0:3]), key=itemgetter(1))[0]
-            candmass = s.group_mass[minindex]*10**10/hubble
+            groupmass = s.group_mass[minindex]*10**10/hubble
+            groupos = s.group_pos[minindex]
             print haloi.replace(basepath,"")
-            print 'group position:',grouppos
-            print 'groupmass: %0.2e' % (groupmass*10**10/hubble)
-            print 'candidate masss: %0.2e' % (candmass)
+            print 'group position:',groupos
+            print 'candidate FOF mass: %0.2e' % (groupmass)
             print "Contatmination Num.:",contamNR[minindex]
             print "Contatmination Mass:",'%0.2e' % (contamMR[minindex]/0.6711)
+
