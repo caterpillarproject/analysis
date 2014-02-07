45fc7e72884ba3012a49d40228253d15c8c8706e
Alexander Ji <alexji@mit.edu>
2014-02-06 19:14:10 -0500

    Profiles code/plotting working

--- profiles/compute_all_profiles.py
+++ profiles/compute_all_profiles.py
@@ -5,17 +5,19 @@ from findhalos.haloutils import get_numsnaps,get_foldername,find_halo_paths
 from findhalos.haloutils import check_last_rockstar_exists
 
 import readhalos.RSDataReader as RDR
+import readhalos.readsubf as readsubf
+import readsnapshots.readids as readids
 import readsnapshots.readsnap as rs
 
 from sets import Set
-['H21047', 'BB', 'Z127', 'P7', 'LN7', 'LX12', 'O4', 'NV3']
+from operator import itemgetter
 
 def get_best_halo_id(outpath,cat):
     fileparts = get_foldername(outpath).split('_')
     hid = int(fileparts[0][1:])
     lx = int(fileparts[5][2:4])
     nv = int(fileparts[7][2])
-
+    
     if hid==241932:
         if lx == 11 and nv == 3:
             return 5563
@@ -41,9 +43,10 @@ def get_best_halo_id(outpath,cat):
     return int(cat[cat['npart']==np.max(cat['npart'])]['id'])
 
 def auto_rarr(rvir,dr=1):
-    return np.arange(0,rvir+dr,dr)/1000. #kpc to Mpc
+    #return np.arange(0,(1.5*rvir)+dr,dr)/1000. #kpc to Mpc
+    return np.logspace(-5,0,100)
 
-def compute_all_profiles(rarr=-1,ictype="BB",levellist=[11,12,13,14],nrvirlist=[3]):
+def compute_all_profiles(subfind=False,rockstar_get_all=False,rarr=-1,ictype="BB",levellist=[11,12,13,14],nrvirlist=[3]):
     if rarr == -1:
         autorflag = True
     else:
@@ -52,13 +55,12 @@ def compute_all_profiles(rarr=-1,ictype="BB",levellist=[11,12,13,14],nrvirlist=[
     rspathlist = []
     for outpath in halopathlist:
         if check_last_rockstar_exists(outpath):
-            #if get_foldername(outpath)[0:7] != 'H242183':# and \
-                    #get_foldername(outpath)[0:7] != 'H140666': #this guy is messed up
             hname = get_foldername(outpath)[0:7]
             if hname == 'H268422' or hname == 'H241932' or \
                     hname[:-1] == 'H21047' or hname == 'H121869':
                 rspathlist.append(outpath)
     for outpath in rspathlist:
+        print "----------------------------------"
         print "computing profile for "+get_foldername(outpath)
         lastsnap = get_numsnaps(outpath)-1
         snapstr = str(lastsnap).zfill(3)
@@ -67,17 +69,40 @@ def compute_all_profiles(rarr=-1,ictype="BB",levellist=[11,12,13,14],nrvirlist=[
         snapIDs = rs.read_block(snapfile,"ID  ",parttype=1)
         argsorted = np.argsort(snapIDs)
         snapPOS = rs.read_block(snapfile,"POS ",parttype=1,doubleprec=False)
-        cat = RDR.RSDataReader(outpath+'/halos',lastsnap,AllParticles=True)
-        haloid = get_best_halo_id(outpath,cat)
-
-        print "rsid %i mvir %3.2e" % (haloid,cat.ix[haloid]['mvir']/cat.h0)
-        haloparts = cat.get_all_particles_from_halo(haloid)
-        halopos = np.array(cat.ix[haloid][['posX','posY','posZ']])
-        halorvir = float(cat.ix[haloid]['rvir']) #h^-1 kpc
+        if subfind:
+            cat = readsubf.subfind_catalog(outpath+'/outputs',lastsnap)
+            bestgroup = min(enumerate(cat.group_contamination_count[0:10]),key=itemgetter(1))[0]
+            substart = cat.group_firstsub[bestgroup]
+            subnum = cat.group_nsubs[bestgroup]
+            
+            subii = np.arange(substart,substart+subnum)
+            submass = cat.sub_mass[subii]*10**10/header.hubble
+            subpos = cat.sub_pos[subii]
+            subvmax = cat.sub_vmax[subii]
+            subnpart= cat.sub_len[subii]
+            sub_id_list = np.where((submass <= 1e13) & (submass >= 5e11))[0]
+            if len(sub_id_list) == 1:
+                subid = sub_id_list[0]+substart
+            else:
+                print "More than one candidate in relevant mass range, skipping..."
+                continue
+            haloparts = readids.subid_file(outpath+'/outputs',lastsnap,cat.group_offset[bestgroup],cat.sub_len[cat.group_firstsub[bestgroup]])
+            haloparts = haloparts.SubIDs
+            halopos = cat.sub_pos[subid,:]
+            halorvir = cat.group_r_crit200[bestgroup]*1000.
+            print "bestgroup %i subid %i mvir %3.2e" % (bestgroup,subid,cat.sub_mass[subid]*10**10/header.hubble)
+        else:
+            cat = RDR.RSDataReader(outpath+'/halos',lastsnap,AllParticles=True)
+            haloid = get_best_halo_id(outpath,cat)
+            print "rsid %i mvir %3.2e" % (haloid,cat.ix[haloid]['mvir']/cat.h0)
+            if rockstar_get_all:
+                haloparts = cat.get_all_particles_from_halo(haloid)
+            else:
+                haloparts = cat.get_particles_from_halo(haloid)
+            halopos = np.array(cat.ix[haloid][['posX','posY','posZ']])
+            halorvir = float(cat.ix[haloid]['rvir']) #h^-1 kpc; rvir is 18pi^2
         if autorflag:
             rarr = auto_rarr(halorvir)
-        #print " ",haloid,len(haloparts),halopos,halorvir
-        #print " ",rarr
         try:
             rhoarr,p03rmin = densityprofile(rarr,snapPOS,argsorted,header,haloparts,halopos,power03=True)
         except IndexError as e:
@@ -90,12 +115,16 @@ def compute_all_profiles(rarr=-1,ictype="BB",levellist=[11,12,13,14],nrvirlist=[
             partmasstab = rs.snapshot_header(snapfile+'.0').massarr
             print "  "+str((np.sum(nbad[2:] * partmasstab[2:]))/(np.sum(nbad * partmasstab)))
             rhoarr,p03rmin = densityprofile(rarr,snapPOS,argsorted,header,goodparts,halopos,power03=True)
-        f = open(outpath+'/rs-halo-profile.dat','w')
-        f.write(str(p03rmin)+"\n")
-        for r,rho in zip(rarr[1:],rhoarr):
+        if subfind:
+            f = open(outpath+'/subf-halo-profile.dat','w')
+        elif rockstar_get_all:
+            f = open(outpath+'/rs-halo-profile-allpart.dat','w')
+        else:
+            f = open(outpath+'/rs-halo-profile.dat','w')
+        f.write(str(p03rmin)+" "+str(halorvir)+"\n")
+        for r,rho in zip(rarr[2:],rhoarr[1:]):
             f.write(str(r)+" "+str(rho)+"\n")
         f.close()
-        print ""
 
 def remove_contaminated_particles(snapfile,haloparts, snapIDs,count_all = False):
     hpartset = Set(haloparts); snapset = Set(snapIDs)
@@ -117,4 +146,7 @@ def remove_contaminated_particles(snapfile,haloparts, snapIDs,count_all = False)
         return nbad,np.array(goodparts)
 
 if __name__=="__main__":
-    compute_all_profiles(levellist=[11,12],nrvirlist=[3])
+    levellist = [11,12]; nrvirlist = [3]
+    #compute_all_profiles(levellist=levellist,nrvirlist=nrvirlist)
+    compute_all_profiles(levellist=levellist,nrvirlist=nrvirlist,subfind=True)
+    #compute_all_profiles(levellist=levellist,nrvirlist=nrvirlist,rockstar_get_all=True)
---
+++ profiles/plot_profiles.py
@@ -0,0 +1,98 @@
+import numpy as np
+import pylab as plt
+import asciitable
+
+from profilefit import NFWprofile,fitNFW
+
+from optparse import OptionParser
+
+def plot_profile(ax,outpath,
+                 subfind=False,rockstar_get_all=False,
+                 plotp03=True,plotrvir=False,plotNFW=False,**kwargs):
+    # First line is p03r(Mpc), rvir (kpc)
+    # Subsequent lines are r(Mpc), rho (10^10 Msun/Mpc^3)
+    if subfind:
+        filename = outpath+'/subf-halo-profile.dat'
+    elif rockstar_get_all:
+        filename = outpath+'/rs-halo-profile-allpart.dat'
+    else:
+        filename = outpath+'/rs-halo-profile.dat'
+    data = np.array(asciitable.read(filename,delimiter=" ",data_start=1))
+    r = 1000.*data['col1']; rho = data['col2']
+    f = open(filename,'r')
+    p03r,rvir = f.readline().split(" ")
+    p03r = 1000.*float(p03r); rvir = float(rvir)
+    f.close()
+
+    ax.plot(r,(r/1000.)**2 * rho,**kwargs)
+    ymin = 10**-1.5; ymax = 10**2.5
+    if plotp03:
+        ax.plot([p03r,p03r],[ymin,ymax],ls='--',**kwargs)
+    if plotrvir:
+        ax.plot([rvir,rvir],[ymin,ymax],'k-.')
+    if plotNFW:
+        good_r = (r > p03r) & (r < rvir)
+        rs,rhos = fitNFW(r[good_r],rho[good_r],x0=[50,5],bounds=[(1,3000),(4,8)])
+        ax.plot(r,(r/1000.)**2 * NFWprofile(r,rs,rhos),ls=':',lw='0.5',**kwargs)
+    plt.xlabel(r'r [$h^{-1}$ kpc]')
+    #plt.ylabel(r'$\rho(r)$ [$10^{10} M_\odot h^3$ Mpc$^{-3}$ Mpc$^{-1}$]')
+    plt.ylabel(r'$r^2 \rho(r)$ [$10^{10} M_\odot$ Mpc$^{-1}$]')
+    plt.xlim([10**-2,10**3])
+    plt.ylim([ymin,ymax])
+    if plotNFW:
+        return rs
+    else:
+        return None
+
+if __name__=="__main__":
+    parser = OptionParser()
+    parser.add_option("--subfind",action="store_true",dest="subfind",default=False,
+                      help="Plot subfind")
+    parser.add_option("--rockstarall",action="store_true",dest="rockstar_get_all",
+                      default=False,
+                      help="Plot rockstar with subhalo particles")
+    parser.add_option("--save",action="store_true",dest="save",default=False,
+                      help="Save a png")
+    options,args = parser.parse_args()
+    subfind = options.subfind
+    rockstar_get_all = options.rockstar_get_all
+    if subfind and rockstar_get_all:
+        exit("ERROR: don't specify both subfind and rockstar_get_all, only do one")
+
+    fig = plt.figure(figsize=(10,10))
+    plt.subplots_adjust(wspace=.3,hspace=.3)
+    haloidlist = ['H268422','H121869','H241932','H21047']
+    #haloidlist = ['H268422']
+    base = "/bigbang/data/AnnaGroup/caterpillar/halos"
+    for i,haloid in enumerate(haloidlist):
+        ax = plt.subplot(2,2,i+1)
+        rs11 = plot_profile(ax,base+'/'+haloid+'/'+haloid+"_BB_Z127_P7_LN7_LX11_O4_NV3",
+                     color='blue',plotp03=True,plotrvir=True,plotNFW=True,
+                     subfind=subfind,rockstar_get_all=rockstar_get_all)
+        rs12 = plot_profile(ax,base+'/'+haloid+'/'+haloid+"_BB_Z127_P7_LN7_LX12_O4_NV3",
+                     color='red',plotp03=True,plotrvir=True,plotNFW=True,
+                     subfind=subfind,rockstar_get_all=rockstar_get_all)
+        if i==0:
+            if subfind:
+                plotlabel = 'Subfind'
+            elif rockstar_get_all:
+                plotlabel = 'Rockstar (subs)'
+            else:
+                plotlabel = 'Rockstar'
+            ax.text(10**-1.8,10**2.2,plotlabel,color='black',fontsize='small')
+        if rs11 != None:
+            ax.text(10**-1.8,10**1.9,r"LX11 $r_s=$%3.2f kpc" % (rs11),color='blue',fontsize='x-small')
+        if rs12 != None:
+            ax.text(10**-1.8,10**1.7,r"LX12 $r_s=$%3.2f kpc" % (rs12),color='red', fontsize='x-small')
+        plt.title(haloid)
+        plt.loglog()
+    if options.save:
+        if subfind:
+            extra = 'subfind'
+        elif rockstar_get_all:
+            extra = 'rockstarsubs'
+        else:
+            extra = 'rockstar'
+        plt.savefig('four_profiles_'+extra+'.png',bbox_tight=True)
+    else:
+        plt.show()
