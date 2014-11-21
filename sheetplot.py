f131b105ae263d020d5c73272ce3fdfb6c263b51
Alexander Ji <alexji@mit.edu>
2014-11-21 13:28:39 -0500

    First shot at analysis pipeline

--- sheetplot.py
+++ sheetplot.py
@@ -5,9 +5,13 @@ import haloutils
 
 def get_haloidlist(sheet):
     if sheet==1:
-        haloidlist = [95289, 95289, 95289,
-                      95289, 95289, 95289,
-                      95289, 95289, 95289]
+        haloidlist = [95289, 1195448, 1725139,
+                      447649, 5320, 581141,
+                      649861, 1130025, 1631506]
+    elif sheet==2:
+        haloidlist = [1599902, 1354437, 94687,
+                      581180, 581180, 581180, 
+                      581180, 581180, 581180]
     else:
         exit("Invalid sheet number")
     assert len(haloidlist) == 9
@@ -22,10 +26,15 @@ def sheetplot(sheetnum,Reader,Plotter,figsize=None,save=True,pdf=False):
     if figsize==None: figsize=(15,15)
     fig,axarr = plt.subplots(3,3,figsize=figsize,
                              sharex=True,sharey=True)
+
     plt.subplots_adjust(wspace=0,hspace=0)
     axlist = axarr.reshape(-1)
     for i,(hid,ax) in enumerate(zip(haloidlist,axlist)):
-        data = Reader(hid)
+        try:
+            data = Reader(hid)
+        except IOError as e:
+            Plotter(ax,hid,fignum=i)
+            continue
         Plotter(ax,data,fignum=i)
     for ax in np.ravel(axarr[0:2,:]):
         ax.set_xlabel('')
@@ -37,3 +46,9 @@ def sheetplot(sheetnum,Reader,Plotter,figsize=None,save=True,pdf=False):
     else:
         plt.show()
 
+
+def sheetcompute(sheetnum,plugin,lx=None):
+    haloidlist = get_haloidlist(sheetnum)
+    for hid in haloidlist:
+        haloutils.get_available_hpaths(hid)
+        #TODO
