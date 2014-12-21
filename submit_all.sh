0e430a753f134b01dae484b735cc1231c8eec621
Alexander Ji <alexji@mit.edu>
2014-12-21 15:40:40 -0500

    Modified run_analysis.py to take an argument describing what to plot

---
+++ submit_all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sbatch submit1.sbatch
+sbatch submit2.sbatch
+sbatch submit3.sbatch
+sbatch submit4.sbatch
+sbatch submit5.sbatch
+sbatch submit6.sbatch
+sbatch submit7.sbatch
+#sbatch submit8.sbatch
