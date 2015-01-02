2494d0b6349a0b2191da995472318bd3e9a1ede2
Alexander Ji <alexji@mit.edu>
2015-01-01 21:32:59 -0500

    Added load_zoom_mtc

--- haloutils.py
+++ haloutils.py
@@ -335,6 +335,8 @@ def load_rscat(hpath,snap,verbose=True,halodir='halos'):
 
 def load_mtc(hpath,verbose=True,halodir='halos',treedir='trees',**kwargs):
     return MTC.MTCatalogue(hpath+'/'+halodir+'/'+treedir,version=4,**kwargs)
+def load_zoom_mtc(hpath,verbose=True,halodir='halos',treedir='trees',**kwargs):
+    return MTC.MTCatalogue(hpath+'/'+halodir+'/'+treedir,version=4,haloids=[load_zoomid(hpath)],**kwargs)
 
 def load_partblock(hpath,snap,block,parttype=-1,ids=-1,hdf5=True):
     #assert check_is_sorted(hpath,snap=snap,hdf5=hdf5),"snap is sorted"
