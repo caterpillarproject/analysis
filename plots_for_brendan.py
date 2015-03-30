28078d8df94867adc946c0664fdca58270824a09
Alexander Ji <alexji@mit.edu>
2015-03-30 13:08:59 -0400

    Added TBTF

--- plots_for_brendan.py
+++ plots_for_brendan.py
@@ -5,6 +5,7 @@ import matplotlib
 import haloutils
 from caterpillaranalysis import *
 from caterpillarplot import *
+from tbtfplugin import *
 
 if __name__=="__main__":
     sheet = 1
@@ -45,3 +46,35 @@ if __name__=="__main__":
         ax.tick_params(which='major',axis='both', color='k', length=6,width=2)
         ax.tick_params(which='minor',axis='both', color='k', length=3,width=2)
     fig.savefig('brendan_Nvmax.png',bbox_inches='tight')
+
+    ## Converge Nvmax
+    Nvmax = NvmaxPlugin()
+    fig = convergeplot(sheet,Nvmax,usehaloname=True)
+    axlist = fig.axes
+    for ax in axlist:
+        ax.tick_params(which='major',axis='both', color='k', length=6,width=2)
+        ax.tick_params(which='minor',axis='both', color='k', length=3,width=2)
+    fig.savefig('brendan_Nvmax.png',bbox_inches='tight')
+
+    ## TBTF
+    TBTF = TBTFPlugin()
+    fig = convergeplot(sheet,TBTF,whichlx=[14],usehaloname=True)
+    axlist = fig.axes
+    for ax in axlist:
+        ax.tick_params(which='major',axis='both', color='k', length=6,width=2)
+        ax.tick_params(which='minor',axis='both', color='k', length=3,width=2)
+    fig.savefig('brendan_TBTFLX14_s1.png',bbox_inches='tight')
+
+    fig = convergeplot(1,TBTF,whichlx=[13],usehaloname=True)
+    axlist = fig.axes
+    for ax in axlist:
+        ax.tick_params(which='major',axis='both', color='k', length=6,width=2)
+        ax.tick_params(which='minor',axis='both', color='k', length=3,width=2)
+    fig.savefig('brendan_TBTFLX13_s1.png',bbox_inches='tight')
+
+    fig = convergeplot(2,TBTF,whichlx=[13],usehaloname=True)
+    axlist = fig.axes
+    for ax in axlist:
+        ax.tick_params(which='major',axis='both', color='k', length=6,width=2)
+        ax.tick_params(which='minor',axis='both', color='k', length=3,width=2)
+    fig.savefig('brendan_TBTFLX13_s2.png',bbox_inches='tight')
