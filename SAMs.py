e2ae4c97dcf556b62b739139ce3aad9b4f3b206c
Alexander Ji <alexji@mit.edu>
2015-05-13 10:53:37 -0400

    SAMs.py including form/mthresh, and main now reads in data

--- SAMs.py
+++ SAMs.py
@@ -3,6 +3,7 @@ import pylab as plt
 import os,subprocess,sys,time
 import cPickle as pickle
 import pandas as pd
+import warnings
 
 import haloutils
 from caterpillaranalysis import PluginBase
@@ -15,17 +16,17 @@ def get_snap_reion(hpath,zreion,justafter=False):
     if justafter: snap_reion += 1
     return snap_reion
 
-class SimpleSAMsPlugin(PluginBase):
-    def __init__(self):
-        super(SimpleSAMsPlugin,self).__init__()
+class SimpleSAMBasePlugin(PluginBase):
+    def __init__(self,verbose=False):
+        super(SimpleSAMBasePlugin,self).__init__()
         self.filename='SimpleSAMs.p'
 
         self.xmin = 0; self.xmax = 1
         self.ymin = 0; self.ymax = 1
-        self.xlabel = 'b/a'
-        self.ylabel = 'c/a'
+        self.xlabel = 'NA'
+        self.ylabel = 'NA'
         self.xlog=False; self.ylog=False
-        self.autofigname='SAMs'
+        self.autofigname='SimpleSAMs'
 
         self.modelnames = {'R1':  'z=8 reionization',
                            'R2': 'z=10 reionization',
@@ -36,6 +37,8 @@ class SimpleSAMsPlugin(PluginBase):
                            'S1': 'Stochastic abundance matching'
                            }
         self.extant = ExtantDataFirstPass()
+        self.mthresh = 3.*10.**8 #h^-1 Msun
+        self.verbose = verbose
 
     def _analyze(self,hpath):
         if not haloutils.check_last_rockstar_exists(hpath):
@@ -64,7 +67,6 @@ class SimpleSAMsPlugin(PluginBase):
         infall_scale = haloutils.get_scale_snap(hpath,np.array(subs['infall_snap']))
         infall_z = haloutils.get_z_snap(hpath,np.array(subs['infall_snap']))
         subs['infall_scale'] = infall_scale; subs['infall_z'] = infall_z
-
         ## setup properties at z=8,10,12 reionization
         subids = np.array(subs['id'])
         base_reionz_labels = ['origid','mvir','vmax'] #columns in MT
@@ -78,18 +80,24 @@ class SimpleSAMsPlugin(PluginBase):
         finfall_labels = ['finfall_'+label for label in base_finfall_labels]
         for finfall_label in finfall_labels:
             subs[finfall_label] = np.zeros(len(subs))+np.nan
-        ## setup properties at formation
+        ## setup properties at formation (half Minfall)
         subs['form_snap'] = np.zeros(len(subs))+np.nan
         base_form_labels = base_reionz_labels
         form_labels = ['form_'+label for label in base_form_labels]
         for form_label in form_labels:
             subs[form_label] = np.zeros(len(subs))+np.nan
+        ## setup properties at formation (mthresh)
+        subs['mthresh_snap'] = np.zeros(len(subs))+np.nan
+        base_mthresh_labels = base_reionz_labels
+        mthresh_labels = ['mthresh_'+label for label in base_mthresh_labels]
+        for mthresh_label in mthresh_labels:
+            subs[mthresh_label] = np.zeros(len(subs))+np.nan
 
         for subid in subids:
             try:
                 mt = mtc[subid]
             except KeyError:
-                #print "WARNING: MTCatalogue does not have sub id {0}".format(subid)
+                warnings.warn("MTCatalogue does not have sub id {0} (mass {1:.2e})".format(subid,subs['mgrav'][subid]))
                 continue
             mb = mt.getMainBranch()
             ## properties at z=8,10,12 reionization
@@ -97,8 +105,8 @@ class SimpleSAMsPlugin(PluginBase):
                 snap_reion = get_snap_reion(hpath,zreion)
                 reion_ii = mb['snap']==snap_reion
                 if np.sum(reion_ii)==0:
+                    if self.verbose: warnings.warn("subid {0:7} Skipping z{1}".format(subid, zreion))
                     continue
-
                 this_data = [float(mb[reion_ii][label][0]) for label in base_reionz_labels]
                 reionz_labels = ['z'+str(zreion)+'_'+label for label in base_reionz_labels]
                 for reionz_label,val in zip(reionz_labels,this_data):
@@ -113,17 +121,32 @@ class SimpleSAMsPlugin(PluginBase):
             for finfall_label,val in zip(finfall_labels,this_data):
                 subs[finfall_label][subid] = val
             ## properties at formation
-            Mform = float(subs['infall_mvir'][subid])/2.
-            form_snap = np.min(mb['snap'][mb['mvir']>Mform])
-            form_ii = mb['snap']==form_snap
-            subs['form_snap'][subid] = form_snap
-            this_data = [float(mb[form_ii][label][0]) for label in base_form_labels]
-            for form_label,val in zip(form_labels,this_data):
-                subs[form_label][subid] = val
-
-        with open(self.get_outfname(hpath),'w') as f:
-            pickle.dump(subs,f)
-
+            Minfall = float(subs['infall_mvir'][subid])
+            if Minfall > 0:
+                Mform = Minfall/2.
+                form_snap = np.min(mb['snap'][mb['mvir']>Mform])
+                form_ii = mb['snap']==form_snap
+                subs['form_snap'][subid] = form_snap
+                this_data = [float(mb[form_ii][label][0]) for label in base_form_labels]
+                for form_label,val in zip(form_labels,this_data):
+                    subs[form_label][subid] = val
+            elif self.verbose:
+                warnings.warn("subid {0:7} Skipping form: Minfall={1:.2e}, M(z=0)={2:.2e}".format(subid, Minfall, subs['mgrav'][subid]))
+            ## properties at mthresh
+            mthresh_ii = mb['mvir']>self.mthresh
+            if np.sum(mthresh_ii)>0:
+                mthresh_snap = np.min(mb['snap'][mthresh_ii])
+                mthresh_ii = mb['snap']==mthresh_snap
+                subs['mthresh_snap'][subid] = mthresh_snap
+                this_data = [float(mb[mthresh_ii][label][0]) for label in base_mthresh_labels]
+                for mthresh_label,val in zip(mthresh_labels,this_data):
+                    subs[mthresh_label][subid] = val
+            elif self.verbose:
+                warnings.warn("subid {0:7} Skipping mthresh: Mmax={1:.2e}".format(subid, np.max(mb['mvir'])))
+    
+            with open(self.get_outfname(hpath),'w') as f:
+                pickle.dump(subs,f)
+                
     def _read(self,hpath):
         with open(self.get_outfname(hpath),'r') as f:
             subs = pickle.load(f)
@@ -133,7 +156,8 @@ class SimpleSAMsPlugin(PluginBase):
         raise NotImplementedError
 
 if __name__=="__main__":
-    hpath = haloutils.get_hpath_lx(5320,14)
-    plug = SimpleSAMsPlugin()
-    subs = plug.read(hpath,recalc=True)
-
+    hid = int(sys.argv[1])
+    lx = int(sys.argv[2])
+    hpath = haloutils.get_hpath_lx(hid,lx)
+    plug = SimpleSAMBasePlugin(verbose=True)
+    subs = plug.read(hpath)
