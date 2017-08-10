27c61ba868386122bd13d5a0b1f915cba637d1bb
Alexander Ji <alexji@mit.edu>
2017-08-10 12:43:31 -0400

    Added new figs

--- plot_ufd_surv_figs.py
+++ plot_ufd_surv_figs.py
@@ -1,5 +1,5 @@
 import numpy as np
-import time,sys
+import time,sys,os
 import pandas as pd
 import cPickle as pickle
 import matplotlib.pyplot as plt
@@ -15,6 +15,7 @@ import haloutils
 
 sys.path.append("./greg_dwarfs")
 import DwarfMethods as dm
+import abundance_matching
 
 from classify_z8_objects import medianscatter
 from classify_z8_objects import allufdtypes, ufdtypescolors, ufdlinestyles
@@ -42,7 +43,7 @@ prop_labels = ["logM (Msun)", "log 2T/|U|", "spin", "vmaxconc", "logdist (kpc/h)
 #            np.arange(1,4,.1)]
 #all_bins_mid = [(bins[1:]+bins[:-1])/2. for bins in all_bins]
 prop_xlims = [(6.5,9), (-0.1,1.5), (0,0.2), (0,20), (1,4)]
-all_bins = [np.arange(6,9,.2), np.arange(-0.1,1.51,.05),
+all_bins = [np.arange(6,9.1,.2), np.arange(-0.1,1.51,.05),
             np.arange(0,.5,.02), np.arange(0,20,.5),
             np.arange(1,4,.1)]
 all_bins_mid = [(bins[1:]+bins[:-1])/2. for bins in all_bins]
@@ -64,26 +65,60 @@ sns.set_style({"xtick.direction":"in","ytick.direction":"in"})
 
 
 
+def hidstr(hid):
+    """ converts halo ID to str Hxxxxxx (copied from haloutils.py) """
+    if type(hid)==int or type(hid)==np.int64: return 'H'+str(hid)
+    if type(hid)==str:
+        if hid[0]=='H': return hid
+        return 'H'+hid
+    raise ValueError("hid must be int or str, is "+str(type(hid)))
 
 def load_all_hids():
-    hpaths = dm.get_hpaths(field=False, lx=14)
-    hids = [haloutils.get_parent_hid(hpath) for hpath in hpaths]
+    #hpaths = dm.get_hpaths(field=False, lx=14)
+    #hids = [haloutils.get_parent_hid(hpath) for hpath in hpaths]
+    hids = \
+        [1631506,  264569, 1725139,  447649,    5320,  581141, 1130025, 1387186,  581180,
+         1354437, 1725272, 1195448, 1292085,  796175,  388476, 1079897,   94638,   95289,
+         1232164, 1422331,  196589, 1268839, 1599988, 1195075, 1631582, 1422429,   65777,
+         1232423,  196078, 1599902,  795802, 1104787]
     return hids
+
+def load_all_zoomids():
+    #hids = load_all_hids()
+    #zoomids = []
+    #for hid in hids:
+    #    hpath = haloutils.get_hpath_lx(hid, 14)
+    #    zoomid = haloutils.load_zoomid(hpath)
+    #    zoomids.append(zoomid)
+    zoomids = \
+        [162271, 222203, 186440, 256463, 161947, 155220, 178802, 150793, 182707, 218773,
+         219939,  31921, 217812, 188364, 291771,  53628, 151604, 187355, 145878, 348676,
+         167590, 230245, 189407, 216985, 188532, 141967,  68893, 182140, 310128, 200806,
+         324544, 168305]        
+    return zoomids
+
+def load_all_ufdids():
+    hids = load_all_hids()
+    ufdids_by_halo = []
+    for hid in hids:
+        with open("UFDSEARCH_Z0/{}_ufdids.pkl".format(hidstr(hid)),"r") as fp:
+            allufdids = pickle.load(fp)
+        ufdids_by_halo.append(allufdids)
+    return ufdids_by_halo
 
 def load_all_data(zin_to_use = None, hid_to_use = None, use_phantoms=True):
     if zin_to_use is None: zin_to_use = [4,6,8,10,12]
 
     h0 = 0.6711
-    hpaths = dm.get_hpaths(field=False, lx=14)
+    hids = load_all_hids()
     all_dfs = []
     for zin in zin_to_use:
-        for hpath in hpaths:
-            hid = haloutils.get_parent_hid(hpath)
+        for hid in hids:
             if hid_to_use is not None:
                 if hid not in hid_to_use:
                     continue
             data = pd.DataFrame(np.load("UFDSEARCH_Z0/{}_z{}haloprops.npy".format(
-                        haloutils.hidstr(hid), zin)))
+                        hidstr(hid), zin)))
             data["hid"] = hid
             data["zin"] = zin
             if not use_phantoms:
@@ -93,6 +128,53 @@ def load_all_data(zin_to_use = None, hid_to_use = None, use_phantoms=True):
     alldata["logmvir"] = np.log10(alldata["mvir"]/h0)
     alldata["logeta"] = np.log10(2*alldata["T/|U|"])
     return alldata
+
+def classify_halos_at_z0(zin, recalc=False):
+    fname = "UFDSEARCH_Z0/classification_z{}.npy".format(zin)
+    if os.path.exists(fname) and not recalc:
+        # load the data
+        classification = np.load(fname)
+        return classification
+    
+    
+    alldata = load_all_data(zin_to_use=[zin], use_phantoms=global_use_phantoms)
+    
+    am = abundance_matching.GarrisonKimmel()
+    LMmin, LMmax = am.stellar_to_halo_mass([1000., 2.e5])
+    
+    hids = load_all_hids()
+    zoomids = load_all_zoomids()
+    ufdids_by_halo = load_all_ufdids()
+    
+    # Classificationify each halo into what object it ends up in
+    # host, classical dSph, four ufd types
+    classification = np.full((len(alldata), 6), False, dtype=bool)
+
+    start = time.time()
+    ii_big = np.array(alldata["extant_maxmass"] > LMmax)
+    for hid, zoomid, allufdids in zip(hids, zoomids, ufdids_by_halo):
+        ii_this = np.array(alldata["hid"] == hid)
+        ii_host = np.array(alldata["mtkey"] == zoomid) & ii_this
+        classification[ii_host, 0] = True
+
+        ii_cdsph = ii_big & (~ii_host) & ii_this
+        classification[:,1][ii_cdsph] = True
+        
+        print hid,0,np.sum(ii_host)
+        print hid,1,np.sum(ii_cdsph)
+
+        for j, ufdids in enumerate(allufdids):
+            ufdids = np.array(ufdids).astype(int)
+            col = j+2
+            _ii_ufd = np.array(map(lambda x: x in ufdids, np.array(alldata["mtkey"])))
+            ii_ufd = np.logical_and(ii_this, _ii_ufd)
+            print hid,col,np.sum(ii_ufd)
+            classification[ii_ufd, col] = True
+
+    print time.time()-start
+    print "Saving to {}".format(fname)
+    np.save(fname, classification)
+    return classification
 
 def histogram_by_halo(alldata, zin, propcol, bins, survtype=None):
     """
@@ -146,7 +228,7 @@ def plot_medscat(ax, x, yarr, label, color, ls):
 
 def make_fig_1(zin=8, alldata=None):
     if alldata is None:
-        alldata = load_all_data(zin_to_use=[zin])
+        alldata = load_all_data(zin_to_use=[zin], use_phantoms=global_use_phantoms)
     fig, axes = plt.subplots(2,3, figsize=(8*3, 8*2))
     for propcol,ax,bins,x,xlabel,slabel,xlim in \
             zip(prop_cols, axes.flat[:-1], all_bins, all_bins_mid, 
@@ -173,7 +255,7 @@ def make_fig_1(zin=8, alldata=None):
 
 def make_fig_2(zin=8, alldata=None):
     if alldata is None:
-        alldata = load_all_data(zin_to_use=[zin])
+        alldata = load_all_data(zin_to_use=[zin], use_phantoms=global_use_phantoms)
     fig, axes = plt.subplots(2,3, figsize=(8*3, 8*2))
     for propcol,ax,bins,x,xlabel,slabel,xlim in \
             zip(prop_cols, axes.flat[:-1], all_bins, all_bins_mid, 
@@ -201,7 +283,7 @@ def make_fig_2(zin=8, alldata=None):
 
 def make_fig_3(zin=8, alldata=None):
     if alldata is None:
-        alldata = load_all_data(zin_to_use=[zin])
+        alldata = load_all_data(zin_to_use=[zin], use_phantoms=global_use_phantoms)
     
     Nbigbins = len(logmass_bigbinsmid)
     iprops = [1,2,3,4]
@@ -256,7 +338,7 @@ def make_fig_3(zin=8, alldata=None):
 
 def make_fig_4(zin=8, alldata=None):
     if alldata is None:
-        alldata = load_all_data(zin_to_use=[zin])
+        alldata = load_all_data(zin_to_use=[zin], use_phantoms=global_use_phantoms)
     
     Nbigbins = len(logmass_bigbinsmid)
     iprops = [1,2,3,4]
@@ -305,7 +387,7 @@ def make_fig_4(zin=8, alldata=None):
 def make_fig_5(zin=8, alldata=None, subs_only=False, hosts_only=False):
     #fig = plot_2d_hist(zin, fiducial_ufdtype, use_phantoms=global_use_phantoms)
     if alldata is None:
-        alldata = load_all_data(zin_to_use=[zin])
+        alldata = load_all_data(zin_to_use=[zin], use_phantoms=global_use_phantoms)
 
     if subs_only and hosts_only:
         raise ValueError("Must specify only one of subs or hosts only")
@@ -359,7 +441,7 @@ def make_fig_6(zin=8, alldata=None, subs_only=False, hosts_only=False):
     #fig = plot_2d_psurv(8, fiducial_ufdtype)
     #fig.savefig("fig6.pdf")
     if alldata is None:
-        alldata = load_all_data(zin_to_use=[zin])
+        alldata = load_all_data(zin_to_use=[zin], use_phantoms=global_use_phantoms)
 
     if subs_only and hosts_only:
         raise ValueError("Must specify only one of subs or hosts only")
@@ -415,7 +497,7 @@ def make_fig_6(zin=8, alldata=None, subs_only=False, hosts_only=False):
 
 def make_fig_7():
     zin_to_use = [4,6,8,10,12]
-    alldata = load_all_data(zin_to_use=zin_to_use)
+    alldata = load_all_data(zin_to_use=zin_to_use, use_phantoms=global_use_phantoms)
     #fig, axes = plt.subplots(len(zin_to_use), len(prop_cols),
     #                         figsize=(8*len(prop_cols), 8*len(zin_to_use))
     fig, axes = plt.subplots(1, len(prop_cols),
@@ -441,7 +523,7 @@ def make_fig_7():
 
 def make_fig_8():
     zin_to_use = [4,6,8,10,12]
-    allz_alldata = load_all_data(zin_to_use=zin_to_use)
+    allz_alldata = load_all_data(zin_to_use=zin_to_use, use_phantoms=global_use_phantoms)
 
     iprops = [1,2,3,4]
     Nprops = len(iprops)
@@ -484,22 +566,133 @@ def make_fig_8():
     cb.set_label("log P(survive as {})".format(fiducial_ufdtype))
     fig.savefig("fig8.pdf")
 
-def make_fig_9():
-    pass
+def make_fig_9(zin=8, alldata=None):
+    """ This is actually Fig 2/3 """
+    if alldata is None:
+        alldata = load_all_data(zin_to_use=[zin], use_phantoms=global_use_phantoms)
+    # 0: host
+    # 1: cdsph
+    # 2-5: maxm, h14m, h14r, h14i
+    propcol = prop_cols[0] #logmvir
+    x = all_bins_mid[0]
+    xlabel = prop_labels[0]
+    xlim = prop_xlims[0]
+    xbins = all_bins[0]
+
+    classification = classify_halos_at_z0(zin)
+    
+    fig, axes = plt.subplots(1,4, figsize=(8*4, 8*1))
+    allharr = histogram_by_halo(alldata, zin, propcol, xbins)
+    
+    ax = axes[0]
+    harr = histogram_by_halo(alldata[classification[:,0]], zin, propcol, xbins)
+    plot_medscat(ax, x, harr/allharr, "host", 'k', '-')
+    
+    ax = axes[1]
+    harr = histogram_by_halo(alldata[classification[:,1]], zin, propcol, xbins)
+    plot_medscat(ax, x, harr/allharr, "classical dSph", 'k', '-')
+    
+    for j, (ufdtype, color, ls) in enumerate(zip(allufdtypes, ufdtypescolors, ufdlinestyles)):
+        classcol = j+2
+        
+        # UFD
+        iiufdsurv = np.array(alldata[ufdtype])
+        #harr = histogram_by_halo(alldata[classification[:,classcol]], zin, propcol, xbins)
+        harr = histogram_by_halo(alldata[iiufdsurv], zin, propcol, xbins)
+        plot_medscat(axes[2], x, harr/allharr, ufdtype, color, ls)
+        
+        # Not UFD, host, or cdSph
+        #ii = (np.sum(classification[:,[0,1,classcol]], axis=1) == 0)
+        #harr = histogram_by_halo(alldata[ii], zin, propcol, xbins)
+        #plot_medscat(axes[3], x, harr/allharr, "not "+ufdtype, color, ls)
+        
+        # In UFD but not main branch progenitor
+        iiufddead = np.logical_and(classification[:,classcol], ~iiufdsurv)
+        harr = histogram_by_halo(alldata[iiufddead], zin, propcol, xbins)
+        plot_medscat(axes[3], x, harr/allharr, "merged into "+ufdtype, color, ls)
+        
+    for ax in axes:
+        ax.set_xlim(xlim)
+        ax.set_ylim(0,1)
+        ax.set_xlabel(xlabel)
+        ax.legend(loc="upper right", fontsize=12)
+        ax.text(.05,.9,"z={}".format(zin), ha='left', transform=ax.transAxes)
+    axes[0].set_ylabel("p(merge into host | M)")
+    axes[1].set_ylabel("p(merge into classical dSph | M)")
+    axes[2].set_ylabel("p(main progenitor of UFD | M)")
+    axes[3].set_ylabel("p(other progenitor of UFD | M)")
+    fig.savefig("fig9_z{}.pdf".format(zin))
+
+def make_fig_10(zin=8, alldata=None):
+    """ This is actually Fig 3 """
+    zin_to_use = [4,6,8,10,12]
+    #allz_alldata = load_all_data(zin_to_use=zin_to_use, use_phantoms=global_use_phantoms)
+
+    # 0: host
+    # 1: cdsph
+    # 2-5: maxm, h14m, h14r, h14i
+    propcol = prop_cols[0] #logmvir
+    x = all_bins_mid[0]
+    xlabel = prop_labels[0]
+    xlim = prop_xlims[0]
+    xbins = all_bins[0]
+
+    fig, axes = plt.subplots(1,4, figsize=(8*4, 8*1))
+    for i, (zin, color, ls) in enumerate(zip(zin_to_use, redshift_colors, redshift_linestyles)):
+        alldata = load_all_data(zin_to_use=[zin], use_phantoms=global_use_phantoms)
+        classification = classify_halos_at_z0(zin)
+        allharr = histogram_by_halo(alldata, zin, propcol, xbins)
+        
+        ax = axes[0]
+        harr = histogram_by_halo(alldata[classification[:,0]], zin, propcol, xbins)
+        plot_medscat(ax, x, harr/allharr, "z={}".format(zin), color, ls)
+        
+        ax = axes[1]
+        harr = histogram_by_halo(alldata[classification[:,1]], zin, propcol, xbins)
+        plot_medscat(ax, x, harr/allharr, "z={}".format(zin), color, ls)
+        
+        ufdtype = "maxm"
+        classcol = 2
+        
+        # UFD
+        iiufdsurv = np.array(alldata[ufdtype])
+        #harr = histogram_by_halo(alldata[classification[:,classcol]], zin, propcol, xbins)
+        harr = histogram_by_halo(alldata[iiufdsurv], zin, propcol, xbins)
+        plot_medscat(axes[2], x, harr/allharr, "{} z={}".format(ufdtype,zin), color, ls)
+        
+        # In UFD but not main branch progenitor
+        iiufddead = np.logical_and(classification[:,classcol], ~iiufdsurv)
+        harr = histogram_by_halo(alldata[iiufddead], zin, propcol, xbins)
+        plot_medscat(axes[3], x, harr/allharr, "{} z={}".format(ufdtype,zin), color, ls)
+        
+    for ax in axes:
+        ax.set_xlim(xlim)
+        ax.set_ylim(0,1)
+        ax.set_xlabel(xlabel)
+        ax.legend(loc="upper left", fontsize=12)
+        #ax.text(.05,.9,"z={}".format(zin), ha='left', transform=ax.transAxes)
+    axes[0].set_ylabel("p(merge into host | M)")
+    axes[1].set_ylabel("p(merge into classical dSph | M)")
+    axes[2].set_ylabel("p(main progenitor of UFD | M)")
+    axes[3].set_ylabel("p(other progenitor of UFD | M)")
+    fig.savefig("fig10.pdf")
 
 if __name__=="__main__":
-    for zin in [4,6,8,10,12]:
-        alldatazin = load_all_data(zin_to_use=[zin], use_phantoms=global_use_phantoms)
-        make_fig_1(zin,alldatazin)
-        make_fig_2(zin,alldatazin)
-        make_fig_3(zin,alldatazin)
-        make_fig_4(zin,alldatazin)
-        make_fig_5(zin,alldatazin)
-        make_fig_5(zin,alldatazin,subs_only=True)
-        make_fig_5(zin,alldatazin,hosts_only=True)
-        make_fig_6(zin,alldatazin)
-        make_fig_6(zin,alldatazin,subs_only=True)
-        make_fig_6(zin,alldatazin,hosts_only=True)
-        plt.close("all")
-    make_fig_7()
-    make_fig_8()
+    make_fig_10()
+    #for zin in [4,6,8,10,12]:
+    #    make_fig_9(zin)
+    #    alldatazin = load_all_data(zin_to_use=[zin], use_phantoms=global_use_phantoms)
+    #    make_fig_1(zin,alldatazin)
+    #    make_fig_2(zin,alldatazin)
+    #    make_fig_3(zin,alldatazin)
+    #    make_fig_4(zin,alldatazin)
+    #    make_fig_5(zin,alldatazin)
+    #    make_fig_5(zin,alldatazin,subs_only=True)
+    #    make_fig_5(zin,alldatazin,hosts_only=True)
+    #    make_fig_6(zin,alldatazin)
+    #    make_fig_6(zin,alldatazin,subs_only=True)
+    #    make_fig_6(zin,alldatazin,hosts_only=True)
+    #    plt.close("all")
+    #make_fig_7()
+    #make_fig_8()
+    plt.close("all")
